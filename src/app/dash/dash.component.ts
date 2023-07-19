2e7e348ca11617923deb6e741c7257e8bf95d4ec
Junglota <20197415@itla.edu.do>
2023-07-19 02:17:15 -0400

    Form en el boton de salir (dash)

--- src/app/dash/dash.component.ts
+++ src/app/dash/dash.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-dash',
@@ -18,5 +19,17 @@ export class DashComponent implements OnInit {
 
   constructor(){}
 
+  //Boton de salir abre un form por ejemplo
+  form(){
+    Swal.fire({
+      title:"Form",
+      html: `<input type="text" id="login" class="swal2-input" placeholder="Nombre de producto">
+      <input type="text" id="password" class="swal2-input" placeholder="Cantidad"><input type="text" id="password" class="swal2-input" placeholder="Categoria">`,
+      confirmButtonText: 'Sign in',
+  focusConfirm: false,
+  preConfirm: () => {
+  }
+    })
+  }
 
 }
